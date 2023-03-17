689eb2625a6ba29d9f863ac272dc19c961b4b51b
dayooyinloye <thedayooyinloye>
2023-03-17 02:38:02 +0000

    there

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * main - entry point
@@ -8,8 +10,8 @@
  */
 int main(void)
 {
-	char message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
+	int c;
+	c =write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
