fc587b25c219c09ff58c0782a1565e255044d68a
dayooyinloye <thedayooyinloye>
2023-03-17 01:18:30 +0000

    temi

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+(* 
+ * Description: Printing a short sentence)?
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle\n");
+	
+	return (0);
+}
