1c8f9c4ec82aa854ad458d27c44e4ba031a945eb
dayooyinloye <thedayooyinloye>
2023-03-23 17:05:49 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digtit of %d is %d and is less than 6 and not 0\n",
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 			n, lastn);
 	}
 	return (0);
