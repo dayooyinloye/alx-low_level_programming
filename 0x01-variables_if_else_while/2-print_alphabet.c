ab120c8dd846dc4539ddc6f8517cb7ab745547e2
dayooyinloye <thedayooyinloye>
2023-03-23 17:31:51 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+(*
+ * Description: print letters of the alphabet)?
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar("\n");
+	return (0);
+}
