0f0f361440b51aace8576fee27d00e4dd9bfe191
dayooyinloye <thedayooyinloye>
2023-04-03 10:59:13 +0000

    print single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+(*
+ * Description: print all single digit numbers)?
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+	{
+		printf((n % 10; + '0');
+	}
+	putchar('\n');
+	return (0);
+}
